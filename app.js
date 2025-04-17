c8080079c1ac989a809bb18e15d5432b39807b7a
Aditya Dubey <hmmho705@gmail.com>
2025-04-18 01:00:47 +0530

    frontend made

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ import ejs from 'ejs';
 
 
 const app = express()
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({
+    extended: true
+}))
 
-
-app.get('/', ()=>{
-    res.send("Works")
+app.get('/', (req, res)=>{
+    res.render('index')
 })
 
 app.listen(3000, ()=>{
