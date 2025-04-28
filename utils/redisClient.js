416d416cbbf151d15bb8fafc1b475e89a970d309
Aditya Dubey <hmmho705@gmail.com>
2025-04-28 15:05:07 +0530

    finalchanges

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 const redisClient = createClient({
-  url: 'redis://redis:6379'
+  url: 'redis://localhost:6379'
 });
 
 redisClient.on('error', (err) => console.error('Redis error:', err));
